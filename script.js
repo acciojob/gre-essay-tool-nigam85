47c924a0aed7fc9641dd9863a0da4b884d10f65a
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-15 13:35:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+const evaluatedText = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
+
+evaluatedText.addEventListener("input", countWords);
+
+function countWords() {
+    // Get the text from the textarea
+    const text = evaluatedText.value.trim();
+
+    // Split the text into words (by spaces) and filter out empty strings
+    const words = text.split(/\s+/).filter(word => word !== '');
+
+    // Update the word count in real-time
+    wordCount.textContent = words.length;
+}
+
+// Initialize word count to 0
+wordCount.textContent = "0";
+
